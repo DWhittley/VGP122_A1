1d2ca581ac86f36eb74f3e099836a6f3eea7ccb9
DWhittley <115023269+DWhittley@users.noreply.github.com>
2022-11-04 14:53:03 -0400

    ongoing work

--- VGP122_A1_Whittley_David/main.cpp
+++ VGP122_A1_Whittley_David/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int credits = 1000; // player initial credits 1000
 void init();
-int deck[52];
+int Deck [260] [260] = {0}; // standard 5 deck shoe in Vegas holds 260 cards.  this will hold card value and suit
+char userInput;
 
-
-enum card_suit 
+enum card_suit
 {
 	S, H, D, C
 };
@@ -30,71 +30,76 @@ int main()
 
 void init()
 {
-	int tempCard = 0;
-	int* ptr = 0;
-
-	for (int i = 0; i < 13; ++i) // populate a playing deck with 4 of each card (A,J,Q,K represented by 1,11,12,13)
-	{
-		for (int j = 0; j < 4; ++j)
-		{
-			deck[(i + j)] = (i + 1);
-			cout << "deck location " << &deck[(i + j)] << "holds the value " << deck[(i + j)] << endl; //initial build checking
-		}
-	}
-
-	for (int i = 0; i < 52; ++i) // move 52 values from deck array (random location) to shuffleDeck array and replace with 0 in deck location
-	{
-		srand(time(0));
-		*ptr = (&deck[(rand() % 52)] * sizeof(int)); // randomly select array cell location in deck incremented by int size (4 bytes)
-		tempCard = ptr; // temporarily store the value at that array location
-
-		if (tempCard == 0) // if there is a zero at that location then the value has already been pulled, so ignore the selection
-			--i; // pre-reduce i so that next iteration has the same i value, thus we get a total of 52 cards in the shuffleDeck array
-		else if (tempCard >= 1 && tempCard <= 52) // make sure only valid data is stored in shuffleDeck
-		{
-			shuffleDeck[(sizeof(shuffleDeck))] = tempCard; // Store the card value in tempCard in first available location in shuffleDeck
-			deck[*ptr] = 0; // store a 0 in the deck location that we pulled the card value from
-			cout << "The shuffled deck location" << i << "has the value " << shuffleDeck[i] << endl;  //initial build checking
-		}
-
-	}
-
+	Shuffle();
 }
+
 
 void Shuffle()
 {
-	Card* temp = nullptr;
-	int rndIndex;
+	int rndCard;
+	int rndSuit;
 
-	for (int i = 0; i < MAXCARDS; ++i)
+	for (int i = 0; i < 260; ++i) // typical vegas 5 deck shoe holds 260 cards
 	{
-		rndIndex = rand() % MAXCARDS;
+		rndCard = (rand() + 1) % 13; // random card between Ace and King
+		rndSuit = (rand() + 1) % 4; // random suit
+		Deck[i][0] = rndCard;
+		Deck[i][1] = rndSuit;
+		cout << "the Deck array has :" << Deck[i][0] << Deck[i][1] << " stored at array location" << i << endl;
+	}
+}
 
-		temp = cards[0];
-		cards[0] = cards[rndIndex];
-		cards[rndIndex] = temp;
+void Game()
+{
+
+	// Ask player if they want to start a new game or continue
+	cout << "Would you like to (C)ontinue your game or (S)tart a new one?" << endl;
+	cin >> userInput;
+	userInput = tolower(userInput);
+
+	if (userInput == 's')
+	{
+		credits = 1000;
+		GamePlay();
+	}
+	else if (userInput == 'c')
+	{
+		GamePlay();
+	}
+	else
+	{
+		Game();
 	}
 }
 
 void GamePlay()
 {
-	char action = 0;
+	int bet1 = 0;
+	int betSplit = 0;
+	card handP1[10] = { 0 }; // array to hold cards dealt to player in A, 2, 3, 4, 5, 6, 7, 8, 9, T, J, Q, K format: note "T" for ten so that we can keep it to "char" array so smaller
+	card handP1Split[10] = { 0 }; // array to hold cards dealt to player split hand if they choose split
+	card handDlr[10] = { 0 }; // array to hold dealer's hand
+
 	
-	// Ask player if they want to start a new game or continue
-
-
 	// ask player what they want to bet on the hand
 
+	while (bet1 <= 0)
+	{
+		cout << "How much would you like to bet? (minimum 1):";
+		cin >> bet1;
+	}
 
 	// deal initial cards for dealer and player
+
+	InitDeal();
+
 
 	// ask player action what they want to do
 	cout << "What would you like to do? (Hit(H), Stand(S), Split(P), Double Down(D), Pass(X): ";
 	
-	cin >> action;
+	cin >> userInput;
 	
-
-	switch (tolower(action))
+	switch (tolower(userInput))
 	{
 	case 'h':
 		// deal method to deal a card (function that is targetable to player hand 1, player hand 2, dealer)
@@ -113,7 +118,43 @@ void GamePlay()
 	case 'x':
 		// half bet returned to player
 		// new deal
+	default:
+		// go back to input
 
 	}
+	void InitDeal()
+	{
+		HandP1[0] = deal();
+		HandDlr[0] = deal();
+		HandDlr[0].up = false;
+		HandP1[1] = deal();
+		HandDlr[1] = deal();
+	}
+	card deal() 
+	{
+		card new_card; //card we will be returning
 
+		new_card.value = 1 + rand() % 13; //makes sure the random number is between 1 and 13 for the value
+		int suit = rand() % 4;
+		switch (suit) { //makes sure the random number is between 1 and 4 for the suit
+		case 0:
+			new_card.suit = S;
+			break;
+		case 1:
+			new_card.suit = H;
+			break;
+		case 2:
+			new_card.suit = D;
+			break;
+		case 3:
+			new_card.suit = C;
+			break;
+		}
+
+		new_card.up = true; //we'll change it later if it's the dealers face down card
+
+		return new_card; //returning the card
+	}
 }
+
+
